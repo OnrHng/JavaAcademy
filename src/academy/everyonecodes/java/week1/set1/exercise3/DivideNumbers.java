24128fab66f411f85b6f6720b7f175b9afd8ef76
OnrHng <hangul07@hacettepe.edu.tr>
2020-05-19 13:22:52 +0200

    adding exercise

--- src/academy/everyonecodes/java/week1/set1/exercise3/DivideNumbers.java
+++ src/academy/everyonecodes/java/week1/set1/exercise3/DivideNumbers.java
@@ -1,6 +1,7 @@
 package academy.everyonecodes.java.week1.set1.exercise3;
 
 public class DivideNumbers {
+
     public static double divideNums(double num1, double num2){
         return num1 / num2;
     }
@@ -8,6 +9,5 @@ public class DivideNumbers {
     public static void main(String[] args) {
         System.out.println("The quotient is : " + divideNums(40.5 , 10.05));
         System.out.println("The quotient is by 0 : " + divideNums(3.0 , 0));
-
     }
 }
