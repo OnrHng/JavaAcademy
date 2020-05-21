c22db08eb5bd17df547dab7ee686d423e0d9cac3
OnrHng <hangul07@hacettepe.edu.tr>
2020-05-21 17:53:05 +0200

    addding set2 exercise3 and test file

--- src/academy/everyonecodes/java/week1/set2/exercise3/SmallestBiggest.java
+++ src/academy/everyonecodes/java/week1/set2/exercise3/SmallestBiggest.java
@@ -2,16 +2,12 @@ package academy.everyonecodes.java.week1.set2.exercise3;
 
 public class SmallestBiggest {
     public static void main(String[] args) {
-        int[] arr = {-1, 4, 3, 0, 5, 2, 10, -9 };
+        int[] arr = {-1, 4, 3, 0, 5, 2, 10, -9, 6};
         System.out.println("Biggest number in the Array is : " + getBiggest(arr));
         System.out.println("Smallest number in the Array is : " + getSmallest(arr));
     }
-    public static boolean isBigger(int num1, int num2) {
-        return num1 > num2;
-    }
-    public static boolean isSmallest(int num1, int num2) {
-        return num1 < num2;
-    }
+    public static boolean isBigger(int num1, int num2) { return num1 > num2; }
+    public static boolean isSmallest(int num1, int num2) { return num1 < num2; }
     public static int getBiggest(int[] arr){
         int biggest = arr[0];
         int i = 0;
