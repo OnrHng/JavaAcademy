837fdfdf407cf61dd7cd6ff9801a3a3c23d9d091
OnrHng <hangul07@hacettepe.edu.tr>
2020-05-28 10:25:34 +0200

    fix bugs set2 exercise5

--- src/academy/everyonecodes/java/week2/set2/exercise5/Application.java
+++ src/academy/everyonecodes/java/week2/set2/exercise5/Application.java
@@ -2,13 +2,14 @@ package academy.everyonecodes.java.week2.set2.exercise5;
 
 public class Application {
     public static void main(String[] args) {
-        ShoeCustomer matildaShoe = new ShoeCustomer(38);
-        ShoeCustomer hanselShoe = new ShoeCustomer(42);
+        ShoeCustomer matildaShoe = new ShoeCustomer(38); // for Matilda's shoe's size
+        ShoeCustomer hanselShoe = new ShoeCustomer(42); // for Hansel's shoe's size
 
-        ShopAssistant shopAssistant = new ShopAssistant();
+        ShopAssistant shopAssistant = new ShopAssistant(); // create new Shop Assistant
+
         System.out.println(" for matilda : ");
-        shopAssistant.findShoeForCustomer(matildaShoe);
+        shopAssistant.findShoeForCustomer(matildaShoe); // check available shoes with Matilda's shoe size(38)
         System.out.println(" for Hansel : ");
-        shopAssistant.findShoeForCustomer(hanselShoe);
+        shopAssistant.findShoeForCustomer(hanselShoe); // check available shoes with Hansel's shoe size(42)
     }
 }
--- src/academy/everyonecodes/java/week2/set2/exercise5/ShopAssistant.java
+++ src/academy/everyonecodes/java/week2/set2/exercise5/ShopAssistant.java
@@ -1,22 +1,32 @@
 package academy.everyonecodes.java.week2.set2.exercise5;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ShopAssistant {
     public void findShoeForCustomer(ShoeCustomer customer) {
-        List<Shoe> avaiblesShoes;
+        // Create available Shoes
+        List<Shoe> availablesShoes;
         Shoe pink = new Shoe(39, "pink");
         Shoe red = new Shoe(40, "red");
         Shoe blue = new Shoe(38, "blue");
-        Shoe black = new Shoe(42, "black");
+        Shoe black = new Shoe(41, "black");
 
-        avaiblesShoes = List.of(pink, red, blue, black);
+        availablesShoes = List.of(pink, red, blue, black);
 
-        for (Shoe shoe : avaiblesShoes) {
+        boolean isShoeOk = false;
+        int shoeIndex = -1; // for getting shoe index in the available shoes
+        for (Shoe shoe : availablesShoes) { // check available shoe size for customer shoe size
+            shoeIndex++;
             if (customer.tryShoe(shoe)) {
-                System.out.println("I will take the " + shoe.getColor() + " one!");
+                isShoeOk = true;
+                break; // if in the available shoe list has customer shoe's size return true and break loop.
             }
+        }
+
+        if (isShoeOk) {
+            // get color using shoeIndex
+            System.out.println("I will take the " + availablesShoes.get(shoeIndex).getColor() + " one!");
+        } else {
             System.out.println("Nope, this one doesn’t fit, sorry!");
         }
     }
