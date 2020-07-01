db559e81673b7b416cca82370b725e233c21fd39
OnrHng <hangul07@hacettepe.edu.tr>
2020-07-01 09:19:56 +0200

    deleted comment statements in test file

--- src/academy/everyonecodes/java/week7/optinalEnums/exercise2/DrawerTest.java
+++ src/academy/everyonecodes/java/week7/optinalEnums/exercise2/DrawerTest.java
@@ -10,14 +10,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class DrawerTest {
 
     Drawer drawer = new Drawer();
-    MonthlyCalendar monthlyCalendar = new MonthlyCalendar();
-    WeeklyCalendar weeklyCalendar = new WeeklyCalendar();
 
     @Test
     void getOutfitForToday() {
-
-//      Month month = monthlyCalendar.askForMonth() ;
-//      DayOfWeek day = weeklyCalendar.askForDayOfWeek();
 
         Outfit outfit = drawer.getOutfitForToday(Month.of(6), DayOfWeek.of(2) );
 
