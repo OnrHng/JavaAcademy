ef0cdac938cfb38de44f6f17ec573c739f56a395
OnrHng <hangul07@hacettepe.edu.tr>
2020-09-18 22:54:23 +0200

    completed optional java mysql connection optional exercise

---
+++ src/academy/everyonecodes/java/dbExample/EmployeeExample.java
@@ -0,0 +1,98 @@
+package academy.everyonecodes.java.dbExample;
+
+import academy.everyonecodes.java.week5.set2.exercise1.FileReader;
+
+import java.nio.file.Path;
+import java.sql.*;
+import java.util.List;
+
+public class EmployeeExample {
+    private Connection connection = null;
+
+    public EmployeeExample() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Setup the connection with the DB
+        connection = DriverManager
+                .getConnection("jdbc:mysql://localhost/javaSqlExample?" +
+                        "user=onr&password=3000&serverTimezone=UTC");
+    }
+
+    public void readEmployee() throws Exception {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select employeenr,name,salary, department from employees order by employeenr");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("employeenr") + " | " + resultSet.getString("name") + " | "
+                    + resultSet.getInt("salary") + " | " + resultSet.getString("department"));
+        }
+    }
+
+    public void insertEmployee() throws SQLException {
+//      buraya csv file oku onu bir liste icine at , ilk ogesini sil ve db ye ekle
+        FileReader reader = new FileReader();
+        Path path = Path.of("src/academy/everyonecodes/java/dbExample/employees.csv");
+        List<String> lines = reader.read(path);
+        lines.remove(0); // delete ilk line
+
+        PreparedStatement preparedStatement = null;
+        int affectedRows = 0;
+        for (String line : lines) {
+            String[] parsedLine = line.split(",");
+
+            preparedStatement = connection
+                    .prepareStatement("insert into  employees (employeenr, name, salary, department) values(?, ?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, Integer.parseInt(parsedLine[0]));
+            preparedStatement.setString(2, parsedLine[1]);
+            preparedStatement.setInt(3, Integer.parseInt(parsedLine[2]));
+            preparedStatement.setString(4, parsedLine[3]);
+
+            preparedStatement.executeUpdate();
+            affectedRows++;
+        }
+
+        System.out.println(affectedRows + " rows inserted!");
+    }
+
+    public void  dropTable() throws SQLException {
+        String dropTable = "DROP TABLE employees ";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(dropTable);
+            System.out.println("Employees table  deleted...");
+        } catch (SQLException e) {
+            System.out.println("Error on drop table!");
+            e.printStackTrace();
+        }
+    }
+
+    public void  createTable() throws SQLException {
+        String myTableName = "CREATE TABLE employees ("
+                + "employeenr int primary key not null,"
+                + "name varchar(20) not null unique,"
+                + "salary int not null,"
+                + "department varchar(32) not null);";
+        try {
+            Statement statement = connection.createStatement();
+            //This line has the issue
+            statement.executeUpdate(myTableName);
+            System.out.println("Table Created");
+        }
+        catch (SQLException e ) {
+            System.out.println("An error has occured on Table Creation");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String []args) throws Exception {
+        //Make sure your import works if you run your code multiple times.
+        // for this first start drop table then create table,
+        // You can not any problem using this way :-)
+
+        EmployeeExample ma = new EmployeeExample();
+        ma.dropTable();
+        ma.createTable();
+        ma.readEmployee();
+        ma.insertEmployee();
+        ma.readEmployee();
+    }
+}
---
+++ src/academy/everyonecodes/java/dbExample/MySQLAccess.java
@@ -0,0 +1,72 @@
+package academy.everyonecodes.java.dbExample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+// You first need to create this table to run the example!
+create table users (
+  id int primary key auto_increment,
+  uname varchar(20) not null unique,
+  email varchar(255) not null unique
+);
+*/
+
+public class MySQLAccess {
+    private Connection connection = null;
+
+    public MySQLAccess() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Setup the connection with the DB
+        connection = DriverManager
+                .getConnection("jdbc:mysql://localhost/javaSqlExample?" +
+                        "user=onr&password=3000&serverTimezone=UTC");
+    }
+
+    public void readUsers() throws Exception {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select id,uname,email from users order by id");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("uname") + " | "
+                    + resultSet.getString("email"));
+        }
+    }
+
+    public int insertUser() throws SQLException {
+        PreparedStatement preparedStatement = connection
+                .prepareStatement("insert into  users (uname, email) values(?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, "Onur");
+        preparedStatement.setString(2, "onur@gmail.com");
+        int affectedRows = preparedStatement.executeUpdate();
+        System.out.println(affectedRows + " rows inserted!");
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        int lastInsertedId = -1;
+        while (resultSet.next()) {
+            lastInsertedId = resultSet.getInt(1);
+            System.out.println("inserted id: " + lastInsertedId);
+        }
+        return lastInsertedId;
+    }
+
+    public void updateUser(int userId, String newUname) throws SQLException {
+        PreparedStatement preparedStatement = connection
+                .prepareStatement("update users set uname = ? where id = ?");
+        preparedStatement.setString(1, newUname);
+        preparedStatement.setInt(2, userId);
+        int affectedRows = preparedStatement.executeUpdate();
+        System.out.println(affectedRows + " rows updated!");
+    }
+
+    public static void main(String []args) throws Exception {
+        MySQLAccess ma = new MySQLAccess();
+        ma.readUsers();
+        int insertedId = ma.insertUser();
+        ma.updateUser(insertedId, "Micha");
+        ma.readUsers();
+    }
+}
